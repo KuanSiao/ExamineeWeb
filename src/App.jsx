0ce8b906de7330cab9985f1e17ce33288d5106bc
keith_liu <hikariyen881230@gmail.com>
2025-12-24 15:26:03 +0800

    reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import { Route,Routes } from "react-router-dom"
-import { Box,Container  } from "@mui/material"
-import { ThemeProvider } from '@mui/material/styles'
-import customerTheme from "./theme/theme"
-import Header from "./components/App/Header"
-import Footer from "./components/App/Footer"
-import Banner from "./components/App/Banner"
-import Login from "./pages/Login"
-import ChooseExamList from "./pages/ChooseExamList"
-import WarningPage from "./pages/WarningPage"
-import Setting from "./pages/Setting"
-import ExamInput from "./pages/ExamInput"
-import Introduction from "./components/App/Introduction"
+import { Route, Routes } from "react-router-dom";
+import { Box, Container, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import customerTheme from "./theme/theme";
+import Header from "./components/App/Header";
+import Footer from "./components/App/Footer";
+import Banner from "./components/App/Banner";
+import Login from "./pages/Login";
+import ChooseExamList from "./pages/ChooseExamList";
+import WarningPage from "./pages/WarningPage";
+import Setting from "./pages/Setting";
+import ExamInput from "./pages/ExamInput";
+import Introduction from "./components/App/Introduction";
 
 function App() {
   return (
     <>
-    <ThemeProvider theme={customerTheme}>
-       <Box 
-           sx={{minHeight:"100vh",
-                display:"flex",
-                flexDirection:"column",
-           }}>
-          <Header/>
-          <Introduction/>
-          <Container  maxWidth="xl" sx={{mt:4, mb:6,flexGrow:1}}>
+      <CssBaseline />
+      <ThemeProvider theme={customerTheme}>
+        <Box
+          sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
+        >
+          <Header />
+          <Introduction />
+          <Container maxWidth="xl" sx={{ mt: 4, mb: 6, flexGrow: 1 }}>
             <Routes>
-               <Route index element={<Login/>}></Route>
-               <Route path="chooseExam" element={<ChooseExamList/>}></Route>
-               <Route path="warningPage" element={<WarningPage/>}></Route>
-               <Route path="setting" element={<Setting/>}></Route>
-               <Route path="ExamInput" element={<ExamInput/>}></Route>
+              <Route index element={<Login />}></Route>
+              <Route path="chooseExam" element={<ChooseExamList />}></Route>
+              <Route path="warningPage" element={<WarningPage />}></Route>
+              <Route path="setting" element={<Setting />}></Route>
+              <Route path="ExamInput" element={<ExamInput />}></Route>
             </Routes>
           </Container>
-          <Footer/>
-       </Box>
-       </ThemeProvider>
+          <Footer />
+        </Box>
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
 export default App;
